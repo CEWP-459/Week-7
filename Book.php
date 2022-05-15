a7ecd17ac110305f7647b94ce3eecd23f2600049
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-15 19:33:15 -0400

    overriding parent class method

--- Book.php
+++ Book.php
@@ -3,4 +3,10 @@
 class Book extends Item
 {
     public $author;
+
+    public function getListingDescription()
+    {
+        return $this->description . " by " . $this->author;
+    }
+
 }
--- oop-example.php
+++ oop-example.php
@@ -11,4 +11,5 @@ echo $item1 -> getListingDescription() . "<br>";
 
 $book1 = new Book();
 $book1 -> description = "Some Description of Book 1"; 
+$book1 -> author = "Becca Fitzpatrick"; 
 echo $book1 -> getListingDescription() . "<br>";
