88eb16f3cfcdb874685dba70481a164392838089
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-15 17:38:39 -0400

    adding methods to a class

--- item.php
+++ item.php
@@ -5,5 +5,13 @@ class Item {
     public $name;
     public $description = "This is a default description!";
 
+    function sayHello () {
+        return "Hello";
+    }
+
+    function getName () {
+        return $this -> name;
+    }
+
 }
 
--- oop-example.php
+++ oop-example.php
@@ -11,3 +11,9 @@ $item2 = new Item();
 $item2 -> name = "Item 2";
 $item2 -> description = "Hi, I am Item 2!";
 var_dump($item2);
+
+$item3 = new Item();
+var_dump($item3 -> sayHello());
+var_dump($item3 -> getName());
+$item3 -> name = "Item 3";
+var_dump($item3 -> getName());
