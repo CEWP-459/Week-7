faf5379fd283bb62dddc662b734cc07f0149ff58
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-15 18:22:53 -0400

    empty constructor

--- item.php
+++ item.php
@@ -5,6 +5,10 @@ class Item {
     public $name;
     public $description = "This is a default description!";
 
+    function __construct () {
+        echo "In the constructor!";
+    }
+
     function sayHello () {
         return "Hello";
     }
--- oop-example.php
+++ oop-example.php
@@ -3,17 +3,3 @@
 require './item.php';
 
 $item1 = new Item();
-$item1 -> name = "Item 1";
-$item1 -> price = 32.99;
-var_dump($item1);
-
-$item2 = new Item();
-$item2 -> name = "Item 2";
-$item2 -> description = "Hi, I am Item 2!";
-var_dump($item2);
-
-$item3 = new Item();
-var_dump($item3 -> sayHello());
-var_dump($item3 -> getName());
-$item3 -> name = "Item 3";
-var_dump($item3 -> getName());
