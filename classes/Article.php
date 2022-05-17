5807331f15379d04f8a7afadc8128bd82bc3b629
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-17 17:41:03 -0400

    insert article with pdo

--- classes/Article.php
+++ classes/Article.php
@@ -84,7 +84,6 @@ class Article
 
         }
 
-
     }
 
     public function delete ($connection) {
@@ -98,5 +97,36 @@ class Article
         return $stmt -> execute();
 
     } 
+
+    public function create ($connection) {
+
+        if ($this -> validate()) {
+
+            $sql = "INSERT INTO article (title, content, published_at)
+                    VALUES (:title, :content, :published_at)";
+
+            $stmt = $connection -> prepare($sql);
+
+            $stmt -> bindValue(':title', $this -> title, PDO::PARAM_STR);
+            $stmt -> bindValue(':content', $this -> content, PDO::PARAM_STR);
+
+            if ($this -> published_at == '') {
+                $stmt -> bindValue(':published_at', null, PDO::PARAM_NULL);
+            } else {
+                $stmt -> bindValue(':published_at', $this -> published_at, PDO::PARAM_STR);
+            }
+
+            if ($stmt -> execute()) {
+                $this -> id = $connection -> lastInsertId();
+                return true;
+            }
+       
+        } else {
+
+            return false;
+
+        }
+
+    }
 
 }
--- new-article.php
+++ new-article.php
@@ -1,8 +1,8 @@
 <?php
 
 ini_set('display_errors', 1); 
-require 'includes/database-connection.php'; 
-require 'includes/article.php'; 
+require 'classes/Database.php'; 
+require 'classes/Article.php'; 
 require 'includes/auth.php'; 
 
 session_start();
@@ -11,54 +11,21 @@ if (!isLoggedIn()) {
     die("Unauthorised!");
 }
 
-$title = '';
-$content = '';
-$published_at = '';
+$article = new Article();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+    $db = new Database();
+    $connection = $db -> getConn();
 
-    $title = $_POST['title'];
-    $content = $_POST['content'];
-    $published_at = $_POST['published_at'];
+    $article -> title = $_POST['title'];
+    $article -> content = $_POST['content'];
+    $article -> published_at = $_POST['published_at'];
 
-    $errors = validateArticle($title, $content, $published_at);
-
-    if (empty($errors)) {
-
-        $connection = getDB();
-
-        $sql = "INSERT INTO article (title, content, published_at) VALUES (?, ?, ?)";
-
-        $stmt = mysqli_prepare($connection, $sql);
-
-        if ($stmt === false) {
-
-            echo mysqli_error($connection);
-
-        } else {
-
-            if ($published_at == '') {
-                $published_at = null;
-            }
-
-            mysqli_stmt_bind_param($stmt, "sss", $title, $content, $published_at);
-
-            if (mysqli_stmt_execute($stmt)) {
-
-                $id = mysqli_insert_id($connection);
-                echo "Inserted record with ID: $id";
-
-                header("Location: article.php?id=$id");
-                exit;
-
-            } else {
-
-                echo mysqli_stmt_error($stmt);
-
-            }
-        }
-
+    if ($article -> create($connection)) {
+        header("Location: ./single-article.php?id={$article->id}");
     }
+
 }
 
 ?>
